5de844fcb64deab2016b73b106b55b3a9f6d8350
Corey Rosamond <corey@ones-n-zeros.com>
2016-06-08 09:48:16 -0700

    Finished the key object for the pallet area

---
+++ editor/object/pallet/object/key.object.php
@@ -0,0 +1,52 @@
+<?php
+
+    namespace Canvas\Editor\Area\Pallet {
+
+        class Key
+        {
+            const group = 0;
+
+            const editor = 1;
+
+            private $collection = [
+                self::group => null,
+                self::editor => null
+            ];
+
+
+            public function __construct( $group, $editor )
+            {
+                $this->setGroup( $group );
+                $this->setEditor( $editor );
+            }
+
+            public function __destruct()
+            {
+                unset(
+                    $this->collection[self::group],
+                    $this->collection[self::editor],
+                    $this->collection
+                );
+            }
+
+            public function groupID()
+            { return "group:{$this->group()}"; }
+
+
+            public function editorID()
+            { return "editor:{$this->group()}:{$this->editor()}"; }
+
+            private function group()
+            { return $this->collection[self::group]; }
+
+            private function editor()
+            { return $this->collection[self::group]; }
+
+
+            private function setGroup( $group )
+            { $this->collection[self::group] = $group; }
+
+            private function setEditor( $editor )
+            { $this->collection[self::editor] = $editor; }
+        }
+    }
